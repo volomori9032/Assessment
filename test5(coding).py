bbfdaf63a7d5bcf9778ab76ca0ece786c6bb3462
Scarycat105 <volomori9032@gmail.com>
2025-03-24 12:35:11 +1300

    meow

--- test5(coding).py
+++ test5(coding).py
@@ -1,36 +1,46 @@
 # importing easygui module
+
 from easygui import *
+
 import sys
 
 
 def warning():
 
-    msgbox("Before you continue,\n A pre-warning that clicking the X will do nothing in most situations.\n If you wish to exit the program preferably wait until you have the option to exit.", title = "Warning.")
+    msgbox(msg = """
+Before you continue,\n
+A pre-warning that clicking the X will do nothing in most situations.\n
+If you wish to exit the program preferably wait until you have the option to exit.
+""")
 
 
 def exit_program():
 
-    quit = buttonbox(msg = "Do you wish to quit?", title = "Quit?", choices = ["Continue", "Quit"])
+    quit = buttonbox(msg = "Quit?", choices = ["Continue", "Quit"])
     if quit == "Quit":
         sys.exit()
 
 
-def userselector():
+def user_selector():
 
-    global username
-    username = enterbox(msg = "Please enter your choice of identifcation:", title = "Username selector.")
-    if username:
-        if ccbox(msg = f"Please confirm you wish, {username}, as your user."): 
-            msgbox(msg = f"Your user is {username}.", title = "Selected user.")
+    global user_name
+    user_name = enterbox(msg = "Please enter identifcation: ")
+    if user_name:
+        if ccbox(msg = f"Please confirm that, {user_name}, is your user."):
+            msgbox(msg = f"Your user is {user_name}.")
 
         else:
-            userselector()
+            user_selector()
 
 
     else:
-        quit_choice = buttonbox(msg = "No username entered\n You must select a user if you wish to continue.\n Do you wish to choose a user or quit?", title = "Quit?", choices = ["Choose user", "Quit"])
+        quit_choice = buttonbox(msg = """
+No username entered\n
+You must select a user if you wish to continue.\n
+Do you wish to choose a user or quit?
+""", choices = ["Choose user", "Quit"])
         if quit_choice == "Choose user":
-            userselector()
+            user_selector()
         
         else:
             sys.exit()
@@ -38,20 +48,37 @@ def userselector():
 
 def main():
 
-    mainpage = buttonbox(msg = f"Welcome {username} to Societas Spiritus's guide.\n (If you couldn't tell, Societas Spiritus is the name of the book and also a group within.)", title = "Welcome!", choices = ["Overview", "Foedus Aquilarum", "Societas Spiritus", "Circulus Aeternitatis", "Maybe Character creation", "Exit"])
+    main_page = buttonbox(msg = f"""
+Welcome {user_name} to Societas Spiritus's guide.
+(If you couldn't tell, Societas Spiritus is the name of the book and also a group within.)\n
+Please select what you would like to learn about.
+""", choices = ["Overview", "F.A", "S.S", "C.A", "Exit"])
 
-    if mainpage == "Overview":
+    if main_page == "Overview":
         msgbox(msg = """
-               Societas Spiritus focuses mainly on the group, Societas Spiritus.\n
-               Societas Spiritus's main goal is to create human animal mutations to use for illegal and dangerous missions that regular humans would struggle with.\n
-               Societas Spiritus has been in running for about 200~+ years, closer to 400~ years. Over that time, two people escaped and created a detectives firm called, Foedus Aquilarum.\n
-               And a sub-group within Foedus Aquilarum called, Circulus Aeternitatis, who attempt to capture the experiements should they be located.
-               """, title = "Societas Spiritus.", ok_button = "Continue.")
+Societas Spiritus focuses mainly on the group, Societas Spiritus.\n
+Societas Spiritus's main goal is to create human animal mutations.\n
+Societas Spiritus has been in running for about 200~+ years, close to 400~ years.\n
+Societas Spiritus ignores ethical and safety concerns,
+leading to many deaths of staff due to trying to defect and stop their crimes.\n
+""", ok_button = "Continue")
+        msgbox(msg = """
+Over the years, two personnel managed to successfully defect,
+starting a detectives firm called, Foedus Aquilarum.\n
+Foedus Aquilarum has been running for about 100~ years.\n
+Foedus Aquilarum has had many encounters with Societas Spiritus,
+many leading in deaths on both sides.\n
+Foedus Aquilarum attempts to destroy Societas Spiritus's sites and facilities. 
+""", ok_button = "Continue")
+        msgbox(msg = """
+One of the defected personnel corrupted and left Foedus Aquilarum, 
+starting an underground organisation called, Circulus Aeternitatis
+        """)
 
-    elif mainpage == "Exit":
+    elif main_page == "Exit":
         sys.exit()
 
 
 warning()
-userselector()
+user_selector()
 main()
