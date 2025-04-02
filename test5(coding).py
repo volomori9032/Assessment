a7d24ca12c55cd2a79578c260d529b2667b6da89
Scarycat105 <volomori9032@gmail.com>
2025-04-02 13:28:38 +1300

    meow

--- test5(coding).py
+++ test5(coding).py
@@ -10,8 +10,8 @@ import sys
 # Variables of titles
 
 MP = "Main page"  # Title for main page
-W = "Warning!"  # Title for warning
 Q = "Quit?"  # Title for quit
+IN = "Incorrect!"  # Title for incorrect passwords
 US = "Select Identification"  # Title for user_selector
 C = "Confirmation"  # Title for confirmation of user
 OV = "Overview"  # Title for overview
@@ -20,42 +20,6 @@ PS = "Password?"  # Title for passwords
 S = "Societas Spiritus"  # Title for S.S
 F = "Foedus Aquilarum"  # Title for F.A
 CA = "Circulus Aeternitatis"  # Title for C.A
-
-
-def warning():
-    """Warns user of potential problems."""
-    msgbox(msg="""
-Before you continue,\n
-A pre-warning that clicking the X will do nothing in most situations.\n
-If you wish to exit the program wait until you have the option to exit.\n
-Makes it easier for both of us.
-""", title=W)
-
-
-def user_selector():
-    """Gathers username or quits."""
-    global user_name
-    user_name = enterbox(msg="Please enter identification: ", title=US)
-
-    if user_name:
-        if ccbox(msg=f"Please confirm, {user_name}, is your user.", title=C):
-            msgbox(msg=f"Your user is {user_name}.", title=C)
-
-        else:
-            user_selector()
-
-    else:
-        quit_choice = buttonbox(msg="""
-No username entered\n
-You must select a user if you wish to continue.\n
-Do you wish to choose a user or quit?
-""", choices=["Choose user", "Quit"], title=Q)
-
-        if quit_choice == "Choose user":
-            user_selector()
-
-        else:
-            sys.exit()
 
 
 def main():
@@ -88,17 +52,46 @@ Please select what you would like to learn about.
         sys.exit()
 
 
+def user_selector():
+    """Gathers username or quits."""
+    global user_name
+    user_name = enterbox(msg="Please enter identification: ", title=US)
+
+    if user_name:
+        if ccbox(msg=f"Please confirm, {user_name}, is your user.", title=C):
+            msgbox(msg=f"Your user is {user_name}.", title=C)
+
+        else:
+            user_selector()
+
+    else:
+        quit_choice = buttonbox(msg="""
+No username entered\n
+You must select a user if you wish to continue.\n
+Do you wish to choose a user or quit?
+""", choices=["Choose user", "Quit"], title=Q)
+
+        if quit_choice == "Choose user":
+            user_selector()
+
+        else:
+            sys.exit()
+
+
 def over_view():
     """Explains the basics of the book."""
-    msgbox(msg="""
+    p1 = msgbox(msg="""
 Societas Spiritus focuses mainly on the group, Societas Spiritus.\n
 S.S's main goal is to create human animal mutations.\n
 S.S has been in running for about 200~+ years, close to 400~ years.\n
 S.S ignores ethical and safety concerns,
 leading to many deaths of staff due to trying to stop their crimes.\n
 """, ok_button="Continue", title=OV)
+    
+    if p1 is None:
+        sys.exit()
 
-    msgbox(msg="""
+    p2 = msgbox(msg="""
 Over the years, two personnel managed to successfully defect,
 starting a detectives firm called, Foedus Aquilarum.\n
 F.A has been running for about 100~ years.\n
@@ -106,8 +99,11 @@ F.A has had many encounters with S.S,
 many leading in deaths on both sides.\n
 F.A attempts to destroy S.S's sites and facilities.
 """, ok_button="Continue", title=OV)
+    
+    if p2 is None:
+        sys.exit()
 
-    msgbox(msg="""
+    p3 = msgbox(msg="""
 One of the defected personnel corrupted and left F.A,
 starting an underground organisation called, Circulus Aeternitatis.\n
 C.A serves to bring a multitude of hostile organizations
@@ -115,6 +111,9 @@ together to locate and capture escaped or injured S.S experiments.\n
 To either recruit or throw into very top secret biddings.\n
 C.A has been in business in for 30 years.
 """, ok_button="Continue", title=OV)
+    
+    if p3 is None:
+        sys.exit()
 
     main()
 
@@ -125,6 +124,9 @@ def foedus_aquilarum():
 
     if fa_password == "League of Eagles":
         fa_main_page()
+
+    elif fa_password is None:
+        sys.exit()
 
     else:
         msgbox(msg="Oops! That is incorrect!")
@@ -139,12 +141,10 @@ What would you like to learn about today?
 """, choices=["placeholder", "Management", "Return"])
 
     if fa == "placeholder":
-
         msgbox(msg="placeholder")
         fa_main_page()
 
     elif fa == "Management":
-
         msgbox(msg="Management")
         fa_main_page()
 
@@ -162,25 +162,26 @@ def societas_spiritus():
     if ss_password == "Societas Spiritus":
         ss_main_page()
 
+    elif ss_password is None:
+        sys.exit()
+
     else:
         msgbox(msg="Oops! That is incorrect!")
         main()
 
 # finish below
 def ss_main_page():
-    """Learn or quit."""
+    """Learn or return."""
     ss = buttonbox(msg="""
 Welcome Societas Spiritus  personnel,\n
 What would you like to learn about today?
 """, choices=["Mutations", "Management", "Return"])
 
     if ss == "Mutations":
-
         msgbox(msg="placeholder")
         ss_main_page()
 
     elif ss == "Management":
-
         msgbox(msg="Management")
         ss_main_page()
 
@@ -197,13 +198,16 @@ def circulus_aeternitatis():
     if ca_password == "30":
         ca_main_page()
 
+    elif ca_password is None:
+        sys.exit()
+
     else:
         msgbox(msg="Oops! That is incorrect!")
         main()
 
 # finish below
 def ca_main_page():
-    """Learn or quit."""
+    """Learn or return."""
     ca = buttonbox(msg="""
 Welcome Circulus Aeternitatis personnel,\n
 What would you like to learn about today?
@@ -245,19 +249,32 @@ C.A always loved to use how long they have been active for as a password.
     else:
         sys.exit
 
-# finish below
+
 def character_creation():
     """Gathers password before proceeding to page or back to main."""
     c_c = passwordbox(msg="Enter all passwords: ")
 
     if c_c == "Societas Spiritus League of Eagles 30":
-        msgbox(msg="Notice to make the character creation if possible")
+        cc_main_page()
+    
+    elif c_c is None:
+        sys.exit()
 
     else:
         msgbox(msg="Yikes! Tried and you failed, maybe learn some more?")
         main()
 
+# finish below
+def cc_main_page():
+    """Create or return."""
+    msgbox(msg=""""
+Welcome to character creator.\n
+Here using the mutations mentioned in S.S's lore,
+you get to make your own!\n
+Well own referring to how it looks.\n
+Not a new mutation!
+""", ok_button="Continue")
 
-warning()
+
 user_selector()
 main()
